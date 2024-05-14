889d1d653eb4a1dbf9f41c3abf910fd7d87a7033
Francisco Pereira Alvarado <gammafp@gmail.com>
2024-05-14 18:50:35 +0200

    update

--- vite/config.dev.mjs
+++ vite/config.dev.mjs
@@ -5,16 +5,16 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+    base: './',
+    plugins: [
+        vue(),
+    ],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
+    },
+    server: {
+        port: 8080
     }
-  },
-  server: {
-      port: 8080
-  }
 })
